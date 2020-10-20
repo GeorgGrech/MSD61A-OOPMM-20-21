0ca78f88e6c6c63a8d2bef811d7dab69e9fac64c
daren-scerri <daren.scerri@mcast.edu.mt>
2020-10-20 13:26:31 +0200

    commit1

---
+++ Lecture Script1/Assets/Scripts/Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triangle : Shape
+{
+    // Start is called before the first frame update
+    Rigidbody2D mytriangle;
+
+    void Start()
+    {
+        mytriangle = this.gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
+        MoveLeft(mytriangle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
