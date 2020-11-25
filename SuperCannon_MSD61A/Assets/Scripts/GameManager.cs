2705b8463b899aeea4973e1b2534bd3aebcc1895
daren-scerri <daren.scerri@mcast.edu.mt>
2020-11-25 17:00:31 +0100

    GameManager

--- SuperCannon_MSD61A/Assets/Scripts/GameManager.cs
+++ SuperCannon_MSD61A/Assets/Scripts/GameManager.cs
@@ -8,7 +8,16 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         GameObject leftBound = GameObject.Find("LeftBoundary");
-        leftBound.transform.position = new Vector3(GameData.XMin - 1, 0f, 0f);
+        leftBound.transform.position = new Vector3(GameData.XMin - 2, 0f, 0f);
+
+        GameObject rightBound = GameObject.Find("RightBoundary");
+        rightBound.transform.position = new Vector3(GameData.XMax + 2, 0f, 0f);
+
+        GameObject topBound = GameObject.Find("TopBoundary");
+        topBound.transform.position = new Vector3(0f, GameData.YMax + 2, 0f);
+
+        GameObject bottomBound = GameObject.Find("BottomBoundary");
+        bottomBound.transform.position = new Vector3(0f, GameData.YMin - 2, 0f);
     }
 
     // Update is called once per frame
